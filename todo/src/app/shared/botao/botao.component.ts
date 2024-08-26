b88d0f6d762382fc1148762766529344e3a5835f
Leonardo Bandeira <lucena.lbd@gmail.com>
2024-08-26 08:55:58 -0300

    :construction: ajusta modal e botões de ação

--- todo/src/app/shared/botao/botao.component.ts
+++ todo/src/app/shared/botao/botao.component.ts
@@ -1,15 +1,21 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-botao',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './botao.component.html',
-  styleUrl: './botao.component.css'
+  styleUrls: ['./botao.component.css']
 })
 export class BotaoComponent {
   @Output() clicked = new EventEmitter<void>();
   @Input() texto: string = 'Nova Tarefa';
+  @Input() tipo: 'add' | 'normal' = 'add';
+
+  get botaoClass() {
+    return `--botao-${this.tipo}`;
+  }
 
   onClick(): void {
     this.clicked.emit();
