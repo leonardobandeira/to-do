8987a950e16ddeb593dc48fdd619c86880e195c3
Leonardo Bandeira <lucena.lbd@gmail.com>
2024-08-26 10:39:40 -0300

    Update content.component.ts

--- todo/src/app/core/template/content/content.component.ts
+++ todo/src/app/core/template/content/content.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { CartaoProgressoComponent } from '../../../components/home/cartao-progresso/cartao-progresso.component';
 import { ModalTarefaComponent } from "../../../components/home/modal-tarefa/modal-tarefa.component";
 import { ContentTitleComponent } from '../../../components/home/content-title/content-title.component';
+import { GraficoComponent } from "../../../components/home/grafico/grafico.component";
 
 @Component({
   selector: 'app-content',
   standalone: true,
   imports: [ContentTitleComponent,
     CartaoProgressoComponent,
-    ModalTarefaComponent],
+    ModalTarefaComponent, GraficoComponent],
   templateUrl: './content.component.html',
   styleUrl: './content.component.css'
 })
